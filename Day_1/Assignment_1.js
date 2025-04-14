0b2cb83962907d49fd66f6a7ba7b42758a3ea937
Sandeep Patil <sandeepvpatil1984@gmail.com>
2025-04-14 18:26:18 +0530

    Variable type of assignment

---
+++ Day_1/Assignment_1.js
@@ -0,0 +1,14 @@
+/*Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`,
+including an uninitialized variable.*/
+
+let firstName = "Sandeep";
+let companyName = "Testleaf";
+let mobileNumber = 1234567890;
+let isAutomation = true;
+let hasPlaywright;
+
+console.log("First Name variable type is: "+typeof(firstName));
+console.log("Company Name variable type is: "+typeof(companyName));
+console.log("Mobile Number variable type is: "+typeof(mobileNumber));
+console.log("isAutomation variable type is: "+typeof(isAutomation));
+console.log("hasPlaywright variable type is: "+typeof(hasPlaywright));
